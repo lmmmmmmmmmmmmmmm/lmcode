9d2e63ea5ead796e409ff108be876aa05c0e63ad
ylm0204 <ylmu709@gmail.com>
2024-07-21 23:10:02 +0800

    feature:优化用户持久层实体

--- admin/src/main/java/com/lm/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/lm/shortlink/admin/dao/entity/UserDO.java
@@ -1,14 +1,11 @@
 package com.lm.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.lm.shortlink.admin.commom.database.BaseDO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 
 @Data
@@ -16,7 +13,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDO {
+public class UserDO extends BaseDO {
 
         /**
          * id
@@ -53,22 +50,6 @@ public class UserDO {
          */
         private Long deletionTime;
 
-        /**
-         * 创建时间
-         */
-        @TableField(fill = FieldFill.INSERT)
-        private Date createTime;
 
-        /**
-         * 修改时间
-         */
-        @TableField(fill = FieldFill.INSERT_UPDATE)
-        private Date updateTime;
-
-        /**
-         * 删除标识 0：未删除 1：已删除
-         */
-        @TableField(fill = FieldFill.INSERT)
-        private Integer delFlag;
 
 }
